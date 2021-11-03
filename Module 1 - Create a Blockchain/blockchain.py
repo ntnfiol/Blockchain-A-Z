5238413450911a1adcfd95d686a34c46bd822916
Devin Capriola <devincapriola@gmail.com>
2021-11-03 18:42:42 -0400

    created the blockchain class

---
+++ Module 1 - Create a Blockchain/blockchain.py
@@ -0,0 +1,16 @@
+# Model 1 - Create a Blockchain
+
+# Importing the libraries
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Part 1 - Building a Blockchain
+
+class Blockchain:   
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+
+# Part 2 - Mining our Blockchain
--- blockchain.py
+++
@@ -1,7 +0,0 @@
-# Model 1 - Create a Blockchain
-
-# Importing the libraries
-import datetime
-import hashlib
-import json
-from flask import Flask, jsonify
