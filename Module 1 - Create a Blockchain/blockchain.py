ca079f433a70c3d58341c048a675bfb6a23a2c36
Devin Capriola <devincapriola@gmail.com>
2021-11-07 01:14:50 -0400

    created mine_block function

--- Module 1 - Create a Blockchain/blockchain.py
+++ Module 1 - Create a Blockchain/blockchain.py
@@ -7,7 +7,6 @@ import json
 from flask import Flask, jsonify
 
 # Part 1 - Building a Blockchain
-
 class Blockchain:   
     def __init__(self):
         self.chain = []
@@ -58,3 +57,27 @@ class Blockchain:
         return True
     
 # Part 2 - Mining our Blockchain
+# Creating a Web App
+app = Flask(__name__)
+
+# Creating a Blockchain
+blockchain = Blockchain()
+
+# Mining a new block
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {
+        'message': 'Congratulations, you just mined a block!',
+        'index': block['index'],
+        'timestamp': block['timestamp'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash']
+    }
+    return jsonify(response), 200
+
+# Getting the full Blockchain
