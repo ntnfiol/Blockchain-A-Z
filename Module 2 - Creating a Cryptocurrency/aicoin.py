1e8504fd400ab9928a17e7863bb2e74a37ba5dd9
Devin Capriola <devincapriola@gmail.com>
2021-11-24 00:46:33 -0500

    created connect_node function

--- Module 2 - Creating a Cryptocurrency/aicoin.py
+++ Module 2 - Creating a Cryptocurrency/aicoin.py
@@ -170,6 +170,20 @@ def add_transaction():
 
 
 # Part 3 - Decentralizing our Blockchain
+# Connecting new nodes
+@app.route("/connect_node", methods=["POST"])
+def connect_node():
+    json = request.get_json()
+    nodes = json.get("nodes")
+    if nodes is None:
+        return "No node", 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {
+        "message": "All the nodes are now connected. The AICoin Blockchain now contains the following nodes:",
+        "total_nodes": list(blockchain.nodes)
+    }
+    return jsonify(response), 201
 
 
 # Running the app
