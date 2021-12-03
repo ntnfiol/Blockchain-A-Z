8e7c585a52bfda7ade298bf856003ded11085865
Devin Capriola <devincapriola@gmail.com>
2021-12-03 15:49:52 -0500

    creating the contract

--- Module 3 - Creating a Smart Contract/aicoin_ico.sol
+++ Module 3 - Creating a Smart Contract/aicoin_ico.sol
@@ -1,8 +1,15 @@
-// AICoin ICO
+// AICoins ICO
 
 // Version of compiler
 pragma solidity >=0.7.0 <0.9.0;
 
 contract AICoinICO {
-    
+    // Inroducing the maximum number of AICoins available for sale
+    uint public max_aicoins = 8100000;
+
+    // Inroducing the USD to AICoins conversion rate
+    uint public usd_to_aicoins = 10000;
+
+    // Inroducing the total number of AICoins that have been bought by the investors
+    uint public total_aicoins_bougth = 0;
 }
